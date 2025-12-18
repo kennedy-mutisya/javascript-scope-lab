ed5c361bb911cf69ed6091856a51394d8c03e0a6
Kennedy Mutisya <nyashtysh@gmail.com>
2025-12-18 20:18:29 +0300

    Updated Menu

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// Global variables
+let burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink = "Strawberry Milkshake";
+
+// Function that adds a new burger
+function addBurger() {
+  let newBurger = "Flatburger";
+  burgers.push(newBurger);
+}
+
+// if statement
+if (true) {
+  // Block-scoped variable
+  let anotherNewBurger = "Maple Bacon Burger";
+
+  // Add to burgers array
+  burgers.push(anotherNewBurger);
+
+  // Function to change the featured drink
+  function changeFeaturedDrink() {
+    featuredDrink = "The JavaShake";
+  }
+}
